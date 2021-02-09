73df550e6e113984330e9218a7ea887aeb991a6c
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-02-09 11:32:33 +0000

    Explain Oauth2 errors better to the user

--- falcon/api_error.go
+++ falcon/api_error.go
@@ -2,11 +2,21 @@ package falcon
 
 import (
 	"fmt"
+	"net/url"
 	"reflect"
+
+	"golang.org/x/oauth2"
 )
 
 // ErrorExplain extracts as much information from the error object as possible and returns as human readable string. This is useful for developers as gofalcon/falcon/client library is swagger generated and various error classes do not adhere to a common interface.
 func ErrorExplain(apiError error) string {
+	if urlError, ok := apiError.(*url.Error); ok {
+	        cause := urlError.Unwrap()
+		if _, ok := cause.(*oauth2.RetrieveError); ok {
+			return apiError.Error()
+		}
+	}
+
 	explained := tryErrorExplain(apiError)
 	if explained != "" {
 		return explained
