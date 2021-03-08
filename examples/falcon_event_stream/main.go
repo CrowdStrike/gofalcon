e4b4bf640f09423bc5cba9d0041075d8e4034025
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-03-08 15:48:04 +0000

    Introduce Streaming API handler in gofalcon library
    Move the ready-made construct from gofalcon examples to the core library.

--- examples/falcon_event_stream/main.go
+++ examples/falcon_event_stream/main.go
@@ -3,20 +3,13 @@ package main
 import (
 	"bufio"
 	"context"
-	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
-	"net/http"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/crowdstrike/gofalcon/falcon"
-	"github.com/crowdstrike/gofalcon/falcon/client"
 	"github.com/crowdstrike/gofalcon/falcon/client/event_streams"
-	"github.com/crowdstrike/gofalcon/falcon/models"
-	"github.com/crowdstrike/gofalcon/falcon/models/streaming_models"
 	"github.com/crowdstrike/gofalcon/pkg/falcon_util"
 )
 
@@ -63,7 +56,7 @@ Falcon Client Secret`)
 
 	availableStreams := response.Payload.Resources
 	for _, availableStream := range availableStreams {
-		stream, err := NewStream(context.Background(), client, appId, availableStream)
+		stream, err := falcon.NewStream(context.Background(), client, appId, availableStream)
 		if err != nil {
 			panic(err)
 		}
@@ -98,114 +91,4 @@ func promptUser(prompt string) string {
 		panic(err)
 	}
 	return strings.TrimSpace(s)
-}
-
-type StreamingHandle struct {
-	ctx    context.Context
-	client *client.CrowdStrikeAPISpecification
-	appId  string
-	stream *models.MainAvailableStreamV2
-	Events chan *streaming_models.EventItem
-	Errors chan StreamingError
-}
-
-func NewStream(ctx context.Context, client *client.CrowdStrikeAPISpecification, appId string, stream *models.MainAvailableStreamV2) (*StreamingHandle, error) {
-	sh := &StreamingHandle{
-		ctx:    ctx,
-		client: client,
-		appId:  appId,
-		stream: stream,
-		Events: make(chan *streaming_models.EventItem),
-		Errors: make(chan StreamingError),
-	}
-	sh.maintainSession()
-	err := sh.open()
-	if err != nil {
-		sh.Close()
-		return nil, err
-	}
-	return sh, nil
-}
-
-func (sh *StreamingHandle) Close() {
-	close(sh.Errors)
-	sh.Errors = nil
-}
-
-type StreamingError struct {
-	Fatal bool
-	Err   error
-}
-
-func (e StreamingError) Error() string {
-	return e.Err.Error()
-}
-
-func (sh *StreamingHandle) maintainSession() {
-	ticker := time.NewTicker(time.Duration(*sh.stream.RefreshActiveSessionInterval*9/10) * time.Second)
-	go func() {
-		defer ticker.Stop()
-		for {
-			select {
-			case <-sh.ctx.Done():
-				return
-			case <-ticker.C:
-				_, err := sh.client.EventStreams.RefreshActiveStreamSession(&event_streams.RefreshActiveStreamSessionParams{
-					AppID:      sh.appId,
-					ActionName: "refresh_active_stream_session",
-					Partition:  0,
-					Context:    sh.ctx,
-				})
-
-				if err != nil {
-					sh.Errors <- StreamingError{
-						Fatal: true,
-						Err:   err,
-					}
-					return
-				}
-			}
-		}
-	}()
-}
-
-func (sh *StreamingHandle) open() error {
-
-	req, err := http.NewRequestWithContext(sh.ctx, "GET", *sh.stream.DataFeedURL, nil)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Accept", "application/json")
-	req.Header.Add("Authorization", "Token "+*sh.stream.SessionToken.Token)
-	req.Header.Add("Connection", "Keep-Alive")
-	req.Header.Add("Date", time.Now().Format(time.RFC1123Z))
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-
-	sh.Events = make(chan *streaming_models.EventItem)
-	go func() {
-		defer resp.Body.Close()
-
-		dec := json.NewDecoder(resp.Body)
-		for dec.More() {
-			var detection streaming_models.EventItem
-			//dec.DisallowUnknownFields()
-			err := dec.Decode(&detection)
-			if err != nil {
-				sh.Errors <- StreamingError{Fatal: false, Err: err}
-			}
-			sh.Events <- &detection
-		}
-		sh.Errors <- StreamingError{
-			Fatal: true,
-			Err:   errors.New("Streaming connection closed"),
-		}
-		close(sh.Events)
-	}()
-
-	return nil
 }
