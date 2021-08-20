972d070bd5718ffc1fd664e1457bc7254513f510
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-08-20 12:41:54 +0100

    Provide workable example on how to do docker login with CrowdStrike registry

---
+++ examples/falcon_registry_token/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/crowdstrike/gofalcon/falcon"
+	"github.com/crowdstrike/gofalcon/falcon/client/unstable"
+	"github.com/crowdstrike/gofalcon/pkg/falcon_util"
+)
+
+func main() {
+	falconClientId := os.Getenv("FALCON_CLIENT_ID")
+	falconClientSecret := os.Getenv("FALCON_CLIENT_SECRET")
+	clientCloud := os.Getenv("FALCON_CLOUD")
+	if falconClientId == "" {
+		falconClientId = falcon_util.PromptUser(`Missing FALCON_CLIENT_ID environment variable. Please provide your OAuth2 API Client ID for authentication with CrowdStrike Falcon platform. Establishing and retrieving OAuth2 API credentials can be performed at https://falcon.crowdstrike.com/support/api-clients-and-keys.
+Falcon Client ID`)
+	}
+	if falconClientSecret == "" {
+		falconClientSecret = falcon_util.PromptUser(`Missing FALCON_CLIENT_SECRET environment variable. Please provide your OAuth2 API Client Secret for authentication with CrowdStrike Falcon platform. Establishing and retrieving OAuth2 API credentials can be performed at https://falcon.crowdstrike.com/support/api-clients-and-keys.
+Falcon Client Secret`)
+	}
+
+	client, err := falcon.NewClient(&falcon.ApiConfig{
+		ClientId:     falconClientId,
+		ClientSecret: falconClientSecret,
+		Cloud:        falcon.Cloud(clientCloud),
+		Context:      context.Background(),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := client.Unstable.ContainerRegistry(&unstable.ContainerRegistryParams{
+		Context: context.Background(),
+	})
+	if err != nil {
+		panic(err)
+	}
+	payload := res.GetPayload()
+	if err = falcon.AssertNoError(payload.Errors); err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", *payload.Resources[0].Token)
+}
