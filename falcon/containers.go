05057cdb9888f031c839c17478120f73d84ad13e
Carlos Matos <carlos.matos@crowdstrike.com>
2025-01-09 13:21:23 -0500

    feat: add additional containers to interact with (#462)
    * feat: add additional containers to interact with
    
    * add helper function that returns all availabl sensor types
    
    * removed KPA, fixed shra uri, and added transformation changes

--- falcon/containers.go
+++ falcon/containers.go
@@ -7,10 +7,14 @@ import (
 type SensorType string
 
 const (
-	SidecarSensor SensorType = "falcon-container"
-	ImageSensor   SensorType = "falcon-imageanalyzer"
-	KacSensor     SensorType = "falcon-kac"
-	NodeSensor    SensorType = "falcon-sensor"
+	SidecarSensor  SensorType = "falcon-container"
+	ImageSensor    SensorType = "falcon-imageanalyzer"
+	KacSensor      SensorType = "falcon-kac"
+	NodeSensor     SensorType = "falcon-sensor"
+	Snapshot       SensorType = "falcon-snapshot"
+	FCSCli         SensorType = "fcs"
+	SHRAController SensorType = "falcon-jobcontroller"
+	SHRAExecutor   SensorType = "falcon-registryassessmentexecutor"
 )
 
 // FalconContainerUploadURI parses cloud string (example: us-1, us-2, eu-1, us-gov-1, etc) and returns a URI for uploading a container image for ImageAssessment.
@@ -44,6 +48,14 @@ func FalconContainerSensorImageURI(falconCloud CloudType, sensorType SensorType)
 		return fmt.Sprintf("%s/falcon-kac/%s/release/falcon-kac", registryFQDN(falconCloud), registryCloud(falconCloud))
 	case NodeSensor:
 		return fmt.Sprintf("%s/falcon-sensor/%s/release/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud))
+	case Snapshot:
+		return fmt.Sprintf("%s/falcon-snapshot/%s/release/cs-snapshotscanner", registryFQDN(falconCloud), registryCloud(falconCloud))
+	case FCSCli:
+		return fmt.Sprintf("%s/fcs/%s/release/cs-fcs", registryFQDN(falconCloud), registryCloud(falconCloud))
+	case SHRAController:
+		return fmt.Sprintf("%s/falcon-selfhostedregistryassessment/release/falcon-jobcontroller", registryFQDN(falconCloud))
+	case SHRAExecutor:
+		return fmt.Sprintf("%s/falcon-selfhostedregistryassessment/release/falcon-registryassessmentexecutor", registryFQDN(falconCloud))
 	default:
 		return fmt.Sprintf("%s/falcon-sensor/%s/release/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud))
 	}
