44247f0019b282872bc1110996a67804986a7411
ffalor <35144141+ffalor@users.noreply.github.com>
2025-11-13 18:07:17 -0600

    add 400 bad request response handling to prevention policies endpoint

--- falcon/client/prevention_policies/get_prevention_policies_responses.go
+++ falcon/client/prevention_policies/get_prevention_policies_responses.go
@@ -31,6 +31,12 @@ func (o *GetPreventionPoliciesReader) ReadResponse(response runtime.ClientRespon
 			return nil, err
 		}
 		return result, nil
+	case 400:
+		result := NewGetPreventionPoliciesBadRequest()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return nil, result
 	case 403:
 		result := NewGetPreventionPoliciesForbidden()
 		if err := result.readResponse(response, consumer, o.formats); err != nil {
@@ -130,6 +136,116 @@ func (o *GetPreventionPoliciesOK) GetPayload() *models.PreventionRespV1 {
 }
 
 func (o *GetPreventionPoliciesOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	// hydrates response header X-CS-TRACEID
+	hdrXCSTRACEID := response.GetHeader("X-CS-TRACEID")
+
+	if hdrXCSTRACEID != "" {
+		o.XCSTRACEID = hdrXCSTRACEID
+	}
+
+	// hydrates response header X-RateLimit-Limit
+	hdrXRateLimitLimit := response.GetHeader("X-RateLimit-Limit")
+
+	if hdrXRateLimitLimit != "" {
+		valxRateLimitLimit, err := swag.ConvertInt64(hdrXRateLimitLimit)
+		if err != nil {
+			return errors.InvalidType("X-RateLimit-Limit", "header", "int64", hdrXRateLimitLimit)
+		}
+		o.XRateLimitLimit = valxRateLimitLimit
+	}
+
+	// hydrates response header X-RateLimit-Remaining
+	hdrXRateLimitRemaining := response.GetHeader("X-RateLimit-Remaining")
+
+	if hdrXRateLimitRemaining != "" {
+		valxRateLimitRemaining, err := swag.ConvertInt64(hdrXRateLimitRemaining)
+		if err != nil {
+			return errors.InvalidType("X-RateLimit-Remaining", "header", "int64", hdrXRateLimitRemaining)
+		}
+		o.XRateLimitRemaining = valxRateLimitRemaining
+	}
+
+	o.Payload = new(models.PreventionRespV1)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
+
+// NewGetPreventionPoliciesBadRequest creates a GetPreventionPoliciesBadRequest with default headers values
+func NewGetPreventionPoliciesBadRequest() *GetPreventionPoliciesBadRequest {
+	return &GetPreventionPoliciesBadRequest{}
+}
+
+/*
+GetPreventionPoliciesBadRequest describes a response with status code 400, with default header values.
+
+Not Found
+*/
+type GetPreventionPoliciesBadRequest struct {
+
+	/* Trace-ID: submit to support if resolving an issue
+	 */
+	XCSTRACEID string
+
+	/* Request limit per minute.
+	 */
+	XRateLimitLimit int64
+
+	/* The number of requests remaining for the sliding one minute window.
+	 */
+	XRateLimitRemaining int64
+
+	Payload *models.PreventionRespV1
+}
+
+// IsSuccess returns true when this get prevention policies bad request response has a 2xx status code
+func (o *GetPreventionPoliciesBadRequest) IsSuccess() bool {
+	return false
+}
+
+// IsRedirect returns true when this get prevention policies bad request response has a 3xx status code
+func (o *GetPreventionPoliciesBadRequest) IsRedirect() bool {
+	return false
+}
+
+// IsClientError returns true when this get prevention policies bad request response has a 4xx status code
+func (o *GetPreventionPoliciesBadRequest) IsClientError() bool {
+	return true
+}
+
+// IsServerError returns true when this get prevention policies bad request response has a 5xx status code
+func (o *GetPreventionPoliciesBadRequest) IsServerError() bool {
+	return false
+}
+
+// IsCode returns true when this get prevention policies bad request response a status code equal to that given
+func (o *GetPreventionPoliciesBadRequest) IsCode(code int) bool {
+	return code == 400
+}
+
+// Code gets the status code for the get prevention policies bad request response
+func (o *GetPreventionPoliciesBadRequest) Code() int {
+	return 400
+}
+
+func (o *GetPreventionPoliciesBadRequest) Error() string {
+	return fmt.Sprintf("[GET /policy/entities/prevention/v1][%d] getPreventionPoliciesBadRequest  %+v", 400, o.Payload)
+}
+
+func (o *GetPreventionPoliciesBadRequest) String() string {
+	return fmt.Sprintf("[GET /policy/entities/prevention/v1][%d] getPreventionPoliciesBadRequest  %+v", 400, o.Payload)
+}
+
+func (o *GetPreventionPoliciesBadRequest) GetPayload() *models.PreventionRespV1 {
+	return o.Payload
+}
+
+func (o *GetPreventionPoliciesBadRequest) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
 
 	// hydrates response header X-CS-TRACEID
 	hdrXCSTRACEID := response.GetHeader("X-CS-TRACEID")
