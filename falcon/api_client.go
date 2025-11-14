4b55938c908a1672b374693df26826191b8e0b31
Frank Falor <35144141+ffalor@users.noreply.github.com>
2025-11-14 14:09:41 -0600

    Update api_client.go

--- falcon/api_client.go
+++ falcon/api_client.go
@@ -16,7 +16,7 @@ import (
 )
 
 // NewClient return newly configured API Client based on configuration supplied by user.
-// NewClient function is preferred entry-point to gofalcon SDK.
+// NewClient function is preferred entry-point to gofalcon SDK.d
 func NewClient(ac *ApiConfig) (*client.CrowdStrikeAPISpecification, error) {
 	if ok, err := credentialsOk(ac); !ok {
 		return nil, err
