842dd150ad2fa9be3282569a0c889ff470596cf6
ffalor <35144141+ffalor@users.noreply.github.com>
2025-12-03 14:53:26 -0600

    add health check endpoint

--- falcon/client/cloud_google_cloud_registration/cloud_google_cloud_registration_client.go
+++ falcon/client/cloud_google_cloud_registration/cloud_google_cloud_registration_client.go
@@ -41,6 +41,7 @@ type ClientService interface {
 	CloudRegistrationGcpPutRegistration(params *CloudRegistrationGcpPutRegistrationParams, opts ...ClientOption) (*CloudRegistrationGcpPutRegistrationOK, error)
 
 	CloudRegistrationGcpUpdateRegistration(params *CloudRegistrationGcpUpdateRegistrationParams, opts ...ClientOption) (*CloudRegistrationGcpUpdateRegistrationOK, error)
+	CloudRegistrationGcpTriggerHealthCheck(params *CloudRegistrationGcpTriggerHealthCheckParams, opts ...ClientOption) (*CloudRegistrationGcpTriggerHealthCheckOK, error)
 
 	SetTransport(transport runtime.ClientTransport)
 }
@@ -278,4 +279,42 @@ func (a *Client) CloudRegistrationGcpUpdateRegistration(params *CloudRegistratio
 // SetTransport changes the transport on the client
 func (a *Client) SetTransport(transport runtime.ClientTransport) {
 	a.transport = transport
+}
+
+/*
+CloudRegistrationGcpTriggerHealthCheck triggers health check scan for g c p registrations
+*/
+func (a *Client) CloudRegistrationGcpTriggerHealthCheck(params *CloudRegistrationGcpTriggerHealthCheckParams, opts ...ClientOption) (*CloudRegistrationGcpTriggerHealthCheckOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewCloudRegistrationGcpTriggerHealthCheckParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "cloud-registration-gcp-trigger-health-check",
+		Method:             "POST",
+		PathPattern:        "/cloud-security-registration-google-cloud/entities/registrations/registration-scans/v1",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &CloudRegistrationGcpTriggerHealthCheckReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*CloudRegistrationGcpTriggerHealthCheckOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for cloud-registration-gcp-trigger-health-check: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
 }
