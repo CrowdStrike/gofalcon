bbb40ba0f9063efca059af7db719c212c301f15b
Wayne Hsieh <wayne.hsieh@crowdstrike.com>
2026-01-05 17:41:32 -0500

    update cloud-aws-registration and cloud-azure-registration clients (#584)
    * feat: update cloud-aws-registration client
    
    * feat: update cloud-azure-registration client
    
    * update

--- falcon/client/cloud_aws_registration/cloud_aws_registration_client.go
+++ falcon/client/cloud_aws_registration/cloud_aws_registration_client.go
@@ -38,7 +38,11 @@ type ClientService interface {
 
 	CloudRegistrationAwsQueryAccounts(params *CloudRegistrationAwsQueryAccountsParams, opts ...ClientOption) (*CloudRegistrationAwsQueryAccountsOK, *CloudRegistrationAwsQueryAccountsMultiStatus, error)
 
+	CloudRegistrationAwsTriggerHealthCheck(params *CloudRegistrationAwsTriggerHealthCheckParams, opts ...ClientOption) (*CloudRegistrationAwsTriggerHealthCheckOK, error)
+
 	CloudRegistrationAwsUpdateAccount(params *CloudRegistrationAwsUpdateAccountParams, opts ...ClientOption) (*CloudRegistrationAwsUpdateAccountOK, *CloudRegistrationAwsUpdateAccountMultiStatus, error)
+
+	CloudRegistrationAwsValidateAccounts(params *CloudRegistrationAwsValidateAccountsParams, opts ...ClientOption) (*CloudRegistrationAwsValidateAccountsOK, error)
 
 	SetTransport(transport runtime.ClientTransport)
 }
@@ -200,6 +204,44 @@ func (a *Client) CloudRegistrationAwsQueryAccounts(params *CloudRegistrationAwsQ
 }
 
 /*
+CloudRegistrationAwsTriggerHealthCheck triggers health check scan for a w s accounts
+*/
+func (a *Client) CloudRegistrationAwsTriggerHealthCheck(params *CloudRegistrationAwsTriggerHealthCheckParams, opts ...ClientOption) (*CloudRegistrationAwsTriggerHealthCheckOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewCloudRegistrationAwsTriggerHealthCheckParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "cloud-registration-aws-trigger-health-check",
+		Method:             "POST",
+		PathPattern:        "/cloud-security-registration-aws/entities/account-scans/v1",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http", "https"},
+		Params:             params,
+		Reader:             &CloudRegistrationAwsTriggerHealthCheckReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*CloudRegistrationAwsTriggerHealthCheckOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for cloud-registration-aws-trigger-health-check: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+/*
 CloudRegistrationAwsUpdateAccount patches a existing account in our system for a customer
 */
 func (a *Client) CloudRegistrationAwsUpdateAccount(params *CloudRegistrationAwsUpdateAccountParams, opts ...ClientOption) (*CloudRegistrationAwsUpdateAccountOK, *CloudRegistrationAwsUpdateAccountMultiStatus, error) {
@@ -235,6 +277,44 @@ func (a *Client) CloudRegistrationAwsUpdateAccount(params *CloudRegistrationAwsU
 	}
 	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
 	msg := fmt.Sprintf("unexpected success response for cloud_aws_registration: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+/*
+CloudRegistrationAwsValidateAccounts validates the a w s account registration status and discover organization child accounts if organization is specified
+*/
+func (a *Client) CloudRegistrationAwsValidateAccounts(params *CloudRegistrationAwsValidateAccountsParams, opts ...ClientOption) (*CloudRegistrationAwsValidateAccountsOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewCloudRegistrationAwsValidateAccountsParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "cloud-registration-aws-validate-accounts",
+		Method:             "POST",
+		PathPattern:        "/cloud-security-registration-aws/entities/account/validate/v1",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http", "https"},
+		Params:             params,
+		Reader:             &CloudRegistrationAwsValidateAccountsReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*CloudRegistrationAwsValidateAccountsOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for cloud-registration-aws-validate-accounts: API contract not enforced by server. Client expected to get an error, but got: %T", result)
 	panic(msg)
 }
 
